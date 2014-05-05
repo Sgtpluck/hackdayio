bbd1a86aade67c6ab5cf4de19e94210fb0865dcd
Kevin Davis <kgdavis@gmail.com>
2014-05-05 01:50:51 -0700

    Presentation queue tests, test cleanup

--- spec/features/hackdays_spec.rb
+++ spec/features/hackdays_spec.rb
@@ -30,7 +30,69 @@ describe "browsing hackdays" do
   end
 
   describe "presentation queue" do
-    it "should show only the hacks that are ready to present"
+    before(:each) do
+      test_sign_in
+      @current_user = User.last
+      @hackday = Fabricate(:hackday)
+      @hack = Fabricate(:hack, hackday: @hackday)
+    end
+
+    it "should let admins move hacks up in the presentation queue" do
+      @hackday.admins << @current_user     
+      hack2 = Fabricate(:hack, hackday: @hackday, presentation_index: 1)
+      @hack.update_attribute(:presentation_index, 2)
+      visit queue_hackday_path(@hackday)
+      within('#hack_' + @hack.id.to_s) do
+        click_on "Move Up"
+      end
+
+      find('.alert.alert-success').text.should have_content("has been moved up")
+      page.should have_css('#hacks>:first-child h3', text: @hack.title)
+    end
+
+    it "should let admins move hacks down in the presentation queue" do
+      @hackday.admins << @current_user     
+      hack2 = Fabricate(:hack, hackday: @hackday, presentation_index: 2)
+      @hack.update_attribute(:presentation_index, 1)
+      visit queue_hackday_path(@hackday)
+      within('#hack_' + @hack.id.to_s) do
+        click_on "Move Down"
+      end
+
+      find('.alert.alert-success').text.should have_content("has been moved down")
+      page.should have_css('#hacks>:last-child h3', text: @hack.title)
+    end
+
+    it "should let admins mark hacks as having been presented" do
+      @hackday.admins << @current_user     
+      @hack.update_attribute(:presentation_index, 1)
+      visit queue_hackday_path(@hackday)
+      within('#hack_' + @hack.id.to_s) do
+        click_on "Presentation Completed"
+      end
+
+      find('.alert.alert-success').text.should have_content("This hack has been presented")
+      page.should_not have_content(@hack.title)
+    end
+
+    it "should restrict queue manipulation to judges" do
+      hack2 = Fabricate(:hack, hackday: @hackday, presentation_index: 2)
+      @hack.update_attribute(:presentation_index, 1)
+      visit queue_hackday_path(@hackday)
+
+      page.should_not have_content("Presentation Completed")
+      page.should_not have_content("Move Down")
+      page.should_not have_content("Move up")
+    end
+
+    it "should show only the hacks that are ready to present" do
+      visit queue_hackday_path(@hackday)
+      page.should_not have_content(@hack.title)
+
+      @hackday.join_queue(@hack)
+      visit queue_hackday_path(@hackday)
+      page.should have_content(@hack.title)
+    end
   end
 
   describe "judges comments" do
